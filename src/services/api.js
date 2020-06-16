81eb79cd4a4d38d99625aae9617e4e4a699af3a2
jessica-tavares <jessicatavaraeseel@gmail.com>
2020-06-16 19:22:22 -0300

    first commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,21 @@
+// Requisições API
 export async function getCategories() {
-  // implement here
+  return new Promise((resolve, reject) => {
+    fetch('https://api.mercadolibre.com/sites/MLB/categories')
+    .then(res => res.json())
+    .then(data => resolve(data))
+  })
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  // implement here
+  const url = '';
+  if (categoryId && query) url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+  if (categoryId) url = `https://api.mercadolibre.com/sites/MLB/search?category=$${categoryId}`;
+  if (query) url = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
+  
+  return new Promise((resolve, reject) => {
+    fetch(url)
+    .then(res => res.json())
+    .then(data => resolve(data))
+  })
 }
