85358ae87aeb7b4631cb7e7b86b67dea62a7fd84
jessica-tavares <jessicatavaraeseel@gmail.com>
2020-06-17 13:48:42 -0300

    aceita cc

--- src/components/product_list/Categories.js
+++ src/components/product_list/Categories.js
@@ -1,32 +1,37 @@
 import React, { Component } from 'react';
 import * as api from '../../services/api';
 
+function CheckInput(elem) {
+  return (
+    <p>
+      <label htmlFor={elem.id}>
+        <input type="radio" id={elem.id} name="categorie" value={elem.name} />
+        {elem.name}
+      </label>
+    </p>
+  );
+}
+
 class Categories extends Component {
   constructor(props) {
     super(props);
     this.state = { categories: '' };
-
   }
 
   componentDidMount() {
     api.getCategories().then((results) => this.setState({ categories: results }));
   }
 
-  CheckInput(elem) {
-    return (
-      <p>
-        <label htmlFor={elem.id}>
-          <input type="radio" id={elem.id} name="categorie" value={elem.name} />
-          {elem.name}
-        </label>
-      </p>
-    );
-  }
-
   render() {
     const { categories } = this.state;
     if (!categories) return <span>Loading...</span>;
-    return <div>{categories.map((elem) => this.CheckInput(elem))}</div>;
+    return (
+      <div>
+        {categories.map((elem) => {
+          return CheckInput(elem);
+        })}
+      </div>
+    );
   }
 }
 
