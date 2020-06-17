e094af85c2c6464ff46ab54e00f06ccf1b02a592
AliceAAtalla <aliceatalla36@gmail.com>
2020-06-17 10:50:13 -0300

    Adding BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import ProductList from './pages/ProductList';
 // import ProductDetails from './pages/ProductDetails';
 // import ShoppingCart from './pages/ShoppingCart';
@@ -8,13 +8,15 @@ import ProductList from './pages/ProductList';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={ProductList} />
-      {/* <Route exact path="/" component={ProductDetails} />
-      <Route exact path="/" component={ShoppingCart} />
-      <Route exact path="/checkout" component={Checkout} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={ProductList} />
+        {/* <Route exact path="/" component={ProductDetails} />
+        <Route exact path="/" component={ShoppingCart} />
+        <Route exact path="/checkout" component={Checkout} />
       <Route component={NotFound} /> */}
-    </Switch>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
