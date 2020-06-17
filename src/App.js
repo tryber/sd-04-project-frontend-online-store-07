cdab0936e69e3a3aaf7e1d7f28c360ff17378ebb
AliceAAtalla <aliceatalla36@gmail.com>
2020-06-17 10:42:11 -0300

    Creation of routes within the App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import ProductList from "./pages/ProductList";
+// import ProductDetails from "./pages/ProductDetails";
+// import ShoppingCart from "./pages/ShoppingCart";
+// import Checkout from "./pages/Checkout";
+// import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route exact path="/" component={ProductList} />
+      {/* <Route exact path="/" component={ProductDetails} />
+      <Route exact path="/" component={ShoppingCart} />
+      <Route exact path="/checkout" component={Checkout} />
+      <Route component={NotFound} /> */}
+    </Switch>
   );
 }
 
