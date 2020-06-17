4c8b7ae4750de45de156ab94b53917cfc4077b53
matheusysd <matheusyurisd@gmail.com>
2020-06-17 10:43:45 -0300

    ProductList página com texto requisito 2

---
+++ src/pages/ProductList.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class ProductList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: '',
+    };
+  }
+
+  render() {
+    const { products } = this.state;
+    if (products === '') {
+      return (
+        <div data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </div>
+      );
+    }
+  }
+}
+
+export default ProductList;
