2ad8493102de083a138bba3ec69faf00e94d8bdd
jessica-tavares <jessicatavaraeseel@gmail.com>
2020-06-18 12:04:06 -0300

    falta passar no teste

--- src/components/product_details/infoProduct.js
+++ src/components/product_details/infoProduct.js
@@ -7,18 +7,13 @@ class infoProduct extends React.Component {
     this.state = { loading: true };
   }
 
-  componentDidMount() {
-    setTimeout(() => this.setState({ loading: false }), 2000);
-  }
-
   render() {
     const { price, thumbnail, title } = this.props.product;
     const availableQuantity = this.props.product.available_quantity;
-    if (this.state.loading) return <h1>Loading...</h1>;
     return (
-      <div className="container" data-testid="product">
-        <div className="product" data-testid="product-detail-name">
-          <h3>{title}</h3>
+      <div className="container">
+        <div className="product">
+          <h3 data-testid="product-detail-name">{title}</h3>
           <img src={thumbnail} alt={title} />
           <p>{`Preço: $${price}`}</p>
           <p>Quantidade Disponível: {availableQuantity}</p>
