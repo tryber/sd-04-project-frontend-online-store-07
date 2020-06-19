5aa957685fc7f2ff0fbd44cc7aa748ffb7a3b7dd
jessica-tavares <jessicatavaraeseel@gmail.com>
2020-06-19 17:54:27 -0300

    voltando com o req 15

--- src/components/product_list/Product.js
+++ src/components/product_list/Product.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Product.css';
+import freeShipping from '../../media/shipp.png';
 import AddToCart from '../../components/addToCartButton';
+
+function FreeShipping() {
+  return (
+    <img
+      className="shippImage"
+      data-testid="free-shipping"
+      src={freeShipping}
+      alt="Free Shipping"
+    />
+  );
+}
 
 class Product extends React.Component {
   render() {
-    const { thumbnail, title, price, id } = this.props.product;
+    const { thumbnail, title, price, id, shipping } = this.props.product;
     return (
       <div className="container" data-testid="product">
         <div className="product">
           <h3>{title}</h3>
-          <img src={thumbnail} alt={title} />
+          <div className="images">
+            <img src={thumbnail} className="image-detail" alt={title} />
+            {(shipping.free_shipping) && FreeShipping() }
+          </div>
           <p>{`Preço: ${price}`}</p>
           <div className="button-container">
             <div className="buttomShop">
